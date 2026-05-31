fa3c17e440ecc4ad8c9df10ed35b8b0611b5a239
Pedro Laerte de Souza Leão Júnior <202604497422@alunos.estacio.br>
2026-05-31 20:15:27 +0000

    vairiaveis declaradas

--- variáveis.c
+++ variáveis.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
 int main () {
-  int idade;
+  int idade;//usar nomes representativos, se vai guardar idade não nomear como X, por exemplo
+  int Idade;// valor inteiro, não aceita valores quebrados
+  int IDADE;
+  int idadE; //variáveis são case sensitive
   int quantidade;
-  float altura;
-  char letra;
-  char nome[];
+  float altura;//numeros flutuantes, quebrados
+  double peso;//valores menores, com mais casas decimais
+  char letra;//caracteres
+  char nome[20];//a variável vai aceitar um array de 19 caracteres sem espaços
+  int teste2; //não deve iniciar com números
   
   
 
