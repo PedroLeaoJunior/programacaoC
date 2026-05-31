edb489fb3549738151bd38e7c0873ab3e7e2a205
Pedro Laerte de Souza Leão Júnior <202604497422@alunos.estacio.br>
2026-05-31 22:02:00 +0000

    atualização insersão scanf

---
+++ 20-inserir_dados_user.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int idade;
+    float altura;
+    char opcao;
+    char nome[20];
+    printf("Digite a sua idade: ");
+    scanf("%d", &idade);
+    printf("A idade é: %d\n", idade);
+
+    printf("Digite a sua altura: ");
+    scanf("%f", &altura);
+    printf("A altura é %.2f\n", altura);
+
+    printf("Digite seu nome: ");
+    scanf("%s", nome);
+    printf("O nome é: %s\n", nome);
+
+    printf("Digite sua opção: ");
+    scanf(" %c", &opcao);
+    printf("A opção seleciona é %c\n", opcao);
+
+    return 0;
+
+}
